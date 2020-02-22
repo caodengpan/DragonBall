14cdd4fc2f19a45b7fe7a3a71112a400f0b76867
caopengpan <1051336507@qq.com>
2020-02-22 21:03:08 +0800

    xx

---
+++ DragonBallU3dProject/Assets/DragonBall/Scripts/TLController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEditor;
+using UnityEngine.Playables;
+
+public class TLController : MonoBehaviour {
+
+    public PlayableDirector director;
+    public List<PlayableAsset> playableAssets;
+    private int index;
+	// Use this for initialization
+	void Start () {
+        
+        director.stopped += OnStoped;
+        director.Play(playableAssets[index]);
+    }
+
+    private void OnStoped(PlayableDirector pd)
+    {
+        index++;
+        if(index< playableAssets.Count)
+        {
+            director.Play(playableAssets[index]);
+        }
+        
+    }
+    [MenuItem("Tools/EditorViewToCamView")]
+    public static void EditorViewToCamView()
+    {
+        Debug.Log(Camera.allCameras.Length);
+        Camera[] cams = SceneView.GetAllSceneCameras();
+        if (cams.Length > 0)
+        {
+            Camera.main.transform.position =cams[0].transform.position;
+            Camera.main.transform.eulerAngles = cams[0].transform.eulerAngles;
+        }
+    }
+
+    // Update is called once per frame
+	void Update () {
+
+
+	}
+}
