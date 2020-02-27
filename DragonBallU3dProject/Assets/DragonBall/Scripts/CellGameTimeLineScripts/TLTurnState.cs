74005e58d448546dc2fc742d8935f8815639bb46
caopengpan <1051336507@qq.com>
2020-02-27 20:59:51 +0800

    xx

---
+++ DragonBallU3dProject/Assets/DragonBall/Scripts/CellGameTimeLineScripts/TLTurnState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TLTurnState : MonoBehaviour {
+
+    [SerializeField]
+    private Material[] matesO;
+    [SerializeField]
+    private Material[] matesT;
+    [SerializeField]
+    private SkinnedMeshRenderer _mR;
+    private void OnEnable()
+    {
+        TurnMeterials(matesT);
+    }
+    // Use this for initialization
+    public void TurnMeterials(Material[] mates)
+    {
+        for (int i = 0; i < mates.Length; i++)
+        {
+            _mR.materials[i].CopyPropertiesFromMaterial(mates[i]);
+        }
+    }
+    private void OnDisable()
+    {
+        TurnMeterials(matesO);
+    }
+}
