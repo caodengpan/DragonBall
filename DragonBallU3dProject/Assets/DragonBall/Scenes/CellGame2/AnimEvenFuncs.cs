df46df5c24f7e9d6e200e82d036ad2a7459aaf83
caopengpan <1051336507@qq.com>
2020-03-01 22:21:38 +0800

    xx

---
+++ DragonBallU3dProject/Assets/DragonBall/Scenes/CellGame2/AnimEvenFuncs.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class AnimEvenFuncs : MonoBehaviour {
+
+    [SerializeField]
+    private Animator _anim;
+    [SerializeField]
+    private Transform hitPos;
+    [SerializeField]
+    private ParticleSystem hitEffect;
+    void AttackFrameTrigger(string str)
+    {
+        
+    }
+    void HitFrameTrigger(string str)
+    {
+        string[] ts = str.Split('_');
+        //for(int i = 0; i < ts.Length; i++)
+        //{
+        //    print(ts[i]);
+        //}
+        Transform tar = null;
+        string str2 = ts[1] + "_" + ts[2];
+        //print(str2);
+        switch (ts[0])
+        {
+            case "behit":
+                tar = hitPos.Find(str2);
+                break;
+            case "block":
+                GameObject go = UnityTool.FindChild(gameObject, str2);
+                tar = go.transform;
+                break;
+        }
+        hitEffect.transform.position = tar.position;
+        hitEffect.Play();
+        print(tar.name);
+    }
+}
---
+++ DragonBallU3dProject/Assets/DragonBall/Scenes/CellGame2/GBTeleportEffect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GBTeleportEffect : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem effect;
+    [SerializeField]
+    private SkinnedMeshRenderer smr;
+    private float timer;
+    private bool canUpdate;
+    private void OnEnable()
+    {
+        effect.Play();
+        canUpdate = true;
+    }
+
+    private void Update()
+    {
+        if (canUpdate)
+        {
+            if (timer < effect.main.duration)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                smr.enabled = true;
+                canUpdate = false;
+            }
+        }
+       
+    }
+    private void OnDisable()
+    {
+        smr.enabled = false;
+        timer = 0;
+        effect.Play();
+    }
+}
