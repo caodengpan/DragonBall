3883736b45c69c5d1cefabf8469f60f62ad0357c
caopengpan <1051336507@qq.com>
2020-03-03 23:08:08 +0800

    xx

---
+++ DragonBallU3dProject/Assets/DragonBall/Scripts/CellGameTimeLineScripts/DBAirBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBAirBall : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Transform initPos;
+	private void OnEnable()
+    {
+        transform.position = initPos.position;
+        transform.LookAt(target);
+    }
+    private void Update()
+    {
+        if(Vector3.Distance(target.position , transform.position) > 3f)
+        {
+            transform.LookAt(target);
+            transform.position += transform.forward * speed*Time.deltaTime;
+        }
+    }
+    private void OnDisable()
+    {
+        transform.position = initPos.position;
+    }
+
+}
