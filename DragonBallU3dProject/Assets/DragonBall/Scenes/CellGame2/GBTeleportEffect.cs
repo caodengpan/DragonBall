489ae31caf876ea5de91484175151cc1f91505e0
caopengpan <1051336507@qq.com>
2020-03-06 22:19:58 +0800

    xx

--- DragonBallU3dProject/Assets/DragonBall/Scenes/CellGame2/GBTeleportEffect.cs
+++ DragonBallU3dProject/Assets/DragonBall/Scenes/CellGame2/GBTeleportEffect.cs
@@ -6,7 +6,7 @@ public class GBTeleportEffect : MonoBehaviour
     [SerializeField]
     private ParticleSystem effect;
     [SerializeField]
-    private SkinnedMeshRenderer smr;
+    private SkinnedMeshRenderer[] smrs;
     private float timer;
     private bool canUpdate;
     private void OnEnable()
@@ -29,7 +29,11 @@ public class GBTeleportEffect : MonoBehaviour
             }
             else
             {
-                smr.enabled = true;
+                for(int i = 0; i < smrs.Length; i++)
+                {
+                    smrs[i].enabled = true;
+                }
+               
                 canUpdate = false;
             }
         }
@@ -37,7 +41,11 @@ public class GBTeleportEffect : MonoBehaviour
     }
     private void OnDisable()
     {
-        smr.enabled = false;
+        for (int i = 0; i < smrs.Length; i++)
+        {
+            smrs[i].enabled = false;
+        }
+           
         timer = 0;
         if (effect != null)
         {
