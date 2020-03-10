b9ebc0c7e91f2064138c093eb283983aab95bd24
caopengpan <1051336507@qq.com>
2020-03-10 22:13:43 +0800

    xx

--- DragonBallU3dProject/Assets/DragonBall/Scripts/CellGameTimeLineScripts/DBAirBall.cs
+++ DragonBallU3dProject/Assets/DragonBall/Scripts/CellGameTimeLineScripts/DBAirBall.cs
@@ -12,30 +12,69 @@ public class DBAirBall : MonoBehaviour {
     private Transform target;
     [SerializeField]
     private Transform initPos;
+    [SerializeField]
+    private ParticleSystem explodeEffect;
+    private Vector3 tarPos;
     private float t;
+    private bool canUpdate;
 	private void OnEnable()
     {
         transform.position = initPos.position;
         transform.LookAt(target);
+        tarPos = target.position;
         t = 0;
         transform.SetParent(null);
+        canUpdate = true;
+    }
+    public void SetValue(Transform tar, Transform init, float trDelta, float sp)
+    {
+        target = tar;
+        initPos = init;
+        trackDelta = trDelta;
+        speed = sp;
+        gameObject.SetActive(true);
     }
     private void Update()
     {
-        if(Vector3.Distance(target.position , transform.position) > 3f)
+        if (canUpdate)
         {
-            
-            transform.position += transform.forward * speed*Time.deltaTime;
+            if (Vector3.Distance(tarPos, transform.position) > 3f)
+            {
+
+                transform.position += transform.forward * speed * Time.deltaTime;
+            }
+            else
+            {
+                canUpdate = false;
+                if (explodeEffect != null)
+                {
+                    StartCoroutine(PlayExplode());
+                }
+            }
+            if (t < trackDelta)
+            {
+                t += Time.deltaTime;
+            }
+            else
+            {
+                t = 0;
+                transform.LookAt(target);
+                tarPos = target.position;
+            }
         }
-        if (t < trackDelta)
+       
+    }
+    IEnumerator PlayExplode()
+    {
+        
+        explodeEffect.gameObject.SetActive(true);
+        float t = 0;
+        while (t<explodeEffect.main.duration)
         {
             t += Time.deltaTime;
+            yield return null;
         }
-        else
-        {
-            t = 0;
-            transform.LookAt(target);
-        }
+        Destroy(gameObject);
     }
     private void OnDisable()
     {
---
+++ DragonBallU3dProject/Assets/DragonBall/Scripts/CellGameTimeLineScripts/DBCreateLitBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DBCreateLitBall : MonoBehaviour {
+
+    
+    public float trackDelta,speed;
+    [SerializeField]
+    private GameObject pref;
+    [SerializeField]
+    private Transform targetTr, initPosTr;
+    private void OnEnable()
+    {
+
+        DBAirBall ball = Instantiate(pref, null).GetComponent<DBAirBall>();
+        ball.SetValue(targetTr, initPosTr,trackDelta,speed);
+    }
+}
